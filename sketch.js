491be78c783e613072f51f67eeebd47ea863a27f
iram-afsha <85054305+iram-afsha@users.noreply.github.com>
2021-06-27 17:59:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var garden,rabbit,apple,orangeL,redL;
+var gardenImg,rabbitImg,carrotImg,orangeImg,redImg;
+
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg = loadImage("apple.png");
+  orangeImg = loadImage("orangeLeaf.png");
+  redImg = loadImage("redImage.png");
+}
+
+
+function setup(){
+  
+  createCanvas(400,400);
+}
+
+
+function draw() {
+  background(0);
+  
+ 
+
+}
+
